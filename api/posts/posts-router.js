b2568170e8b783e3d4cf1c33902b3382b1da52e3
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-03 12:38:53 -0700

    router set connected to server and httpie is working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,11 @@
 // implement your posts router here
+// GET	/api/posts	Returns an array of all the post objects contained in the database
+const express = require('express')
+const router = express.Router();
+const Post = require('./posts-model');
+
+//END POINTS
+
+
+// DEFAULT EXPORT
+module.exports = router
