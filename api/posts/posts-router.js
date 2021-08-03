fc9d2c7851f90c4e576c5c28370e26b280fec949
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-03 12:51:04 -0700

    GET	/api/posts passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,6 +5,16 @@ const router = express.Router();
 const Post = require('./posts-model');
 
 //END POINTS
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(500).json({
+            message: "The posts information could not be retrieved"
+        })
+    }
+})
 
 
 // DEFAULT EXPORT
