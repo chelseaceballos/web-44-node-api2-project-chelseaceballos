3fe0a7bd9cb1eee636e10ec05256bc266173c81c
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-03 17:03:47 -0700

    followed solution walkthrough and could not pass last two tests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -114,18 +114,23 @@ router.delete('/:id', async (req, res) => {
   }
 })
 
-router.get('/:id/comments', (req, res) => {
-    Post.findPostComments(req.params.id)
-    .then(comments =>{
-        if (comments > 0) {
-            res.status(200).json(comments)
-        } else {
-            res.status(404).json({message: "The post with the specified ID does not exist"})
-        }
+router.get('/:id/messages', async (req, res) => {
+   try{ 
+       const post = await Post.findById(req.params.id)
+       if (!post) {
+           res.status(404).json({message: "The post with the specified ID does not exist"})
+       } else {
+           const messages = await Post.findPostComments(req.params.id)
+           res.json(messages)
+       }
+       console.log(stuff)
+   } catch (err) {
+    res.status(500).json({
+        message: "The comments information could not be retrieved",
+        err: err.message,
+        stack: err.stack,
     })
-    .catch(error => {
-        res.status(500).json({ message: "The comments information could not be retrieved" })
-    })
+   }
 })
 
 // DEFAULT EXPORT
